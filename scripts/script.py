01c9f397b3ad5f37b7c4f2d82f015bd812ba0dd9
Dylan Castillo <dylanjcastillo@gmail.com>
2022-09-04 23:42:48 +0200

    Fix data

--- scripts/script.py
+++ scripts/script.py
@@ -4,6 +4,6 @@ import pandas as pd
 from pyodide.http import open_url
 
 # Create sample dataframe
-url = "https://raw.githubusercontent.com/dylanjcastillo/random/main/pyscript/data/iris_modified.csv"
+url = "https://raw.githubusercontent.com/dylanjcastillo/pandas-cheatsheet/main/data/iris_modified.csv"
 df = pd.read_csv(open_url(url))
 df_initial = df.copy()
